23b5c15453fdf82e47c53a3ae5d12a5c8d22fc11
liint <48611109+liint@users.noreply.github.com>
2025-10-14 15:55:01 -0600

    Remaking Lab3Task2 directory

--- Lab3Task2/mazeGraph.py
+++
@@ -1,5 +0,0 @@
-from src.graphClass import Graph
-
-class MazeGraph(Graph):
-    def __init__(self, graph_dict=None, locations=None):
-        super().__init__(graph_dict)
--- Lab3Task2/mazeProblem.py
+++
@@ -1,9 +0,0 @@
-from src.problemClass import Problem
-
-class MazeProblem(Problem):
-
-    def __init__(self, initial, goal, graph):
-        super().__init__(initial, goal)
-        self.graph = graph
-
-    
