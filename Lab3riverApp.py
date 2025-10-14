6c987e48b121a9ccffcc70556d7a5ed6049f3af1
Zachary Lapointe <zachlap64@gmail.com>
2025-10-13 22:02:56 -0600

    again

--- Lab3riverApp.py
+++ Lab3riverApp.py
@@ -134,9 +134,9 @@ def main():
         st.info(f"The Agent goal is: {BFSagent1.goal} .")
         #BFSagent1("LLLL")
         st.write("Solution (a sequence of actions) from the initial state to a goal:")
-        t = []
-        t= BFSagent1("LLLL")
-        st.write(t)
+        
+        st.BFSagent1("LLLL")
+        #st.write(t)
         drawBtn(rp1,BFSagent1,nodeColors)
     
             
