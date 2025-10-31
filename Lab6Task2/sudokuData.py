71396b4548707e3d7c8f882ae7c2afcfb3e633bf
liint <48611109+liint@users.noreply.github.com>
2025-10-31 11:31:45 -0600

    task 2 completed

---
+++ Lab6Task2/sudokuData.py
@@ -0,0 +1,27 @@
+ROWS = list("ABCDEFGHI")
+COLUMNS = range(1,10)
+
+FILLED = {'A2':1,'A8':6, 
+          'B1':3,'B3':9,'B7':1,'B9':5,
+          'C2':8,'C4':3,'C6':5,'C8':7,
+          'D3':2,'D5':7,'D7':8,
+          'E4':6,'E6':8,
+          'F3':8,'F5':9,'F7':2,
+          'G2':2,'G4':4,'G6':1,'G8':9,
+          'H1':9,'H3':4,'H7':6,'H9':1,
+          'I2':3,'I8':8}
+
+SQUARE1 = ['A1','A2','A3','B1','B2','B3','C1','C2','C3']
+SQUARE2 = ['A4','A5','A6','B4','B5','B6','C4','C5','C6']
+SQUARE3 = ['A7','A8','A9','B7','B8','B9','C7','C8','C9']
+SQUARE4 = ['D1','D2','D3','E1','E2','E3','F1','F2','F3']
+SQUARE5 = ['D4','D5','D6','E4','E5','E6','F4','F5','F6']
+SQUARE6 = ['D7','D8','D9','E7','E8','E9','F7','F8','F9']
+SQUARE7 = ['G1','G2','G3','H1','H2','H3','I1','I2','I3']
+SQUARE8 = ['G4','G5','G6','H4','H5','H6','I4','I5','I6']
+SQUARE9 = ['G7','G8','G9','H7','H8','H9','I7','I8','I9']
+ASTERISK= ['B5','C3','C7','E2','E5','E8','G3','G7','H5']
+
+GROUPS = [SQUARE1,SQUARE2,SQUARE3,SQUARE4,SQUARE5,SQUARE6,SQUARE7,SQUARE8,SQUARE9,ASTERISK]
+
+
