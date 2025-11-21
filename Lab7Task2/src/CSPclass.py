2ab614e2847032676f702f500edf42716cb9b6a5
liint <48611109+liint@users.noreply.github.com>
2025-11-21 12:51:10 -0700

    Update CSPclass.py

--- Lab7Task2/src/CSPclass.py
+++ Lab7Task2/src/CSPclass.py
@@ -12,6 +12,7 @@ class CSPBasic(Problem):
         self.initial = ()
         self.curr_domains = None
         self.nassigns = 0
+        self.result = None
 
       # These are for constraint propagation
 
