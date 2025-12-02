0c79d3fb0bce96f05d340328b0f5b05a365cd1f8
liint <48611109+liint@users.noreply.github.com>
2025-12-02 09:05:41 -0700

    added human_player()

--- src/players.py
+++ src/players.py
@@ -8,3 +8,9 @@ def smart_player(search_algorithm):
 def random_player(game, state):
     """A game player who  uses random choice to do a next move"""
     return random.choice(list(game.actions(state)))
+
+def human_player(game,state):
+    move = int(input("Enter the amount you'd like to add: "))
+    while move not in game.actions():
+        move = int(input(f"Invalid input, please add one of {game.actions()}: "))
+    return move
